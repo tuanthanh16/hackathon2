8d6586a7fb303b7e62ffad0c321b6e4eca002cd7
TuanVu <tuanthanh16@gmail.com>
2022-09-30 14:50:46 +0200

    update eventClass.js

--- src/js/eventClass.js
+++ src/js/eventClass.js
@@ -12,13 +12,51 @@ class api_event {
     if (this.feat) {
       //this is featured event
       eventElement.className = "featevent";
-      eventElement.innerHTML =
-        '<div class="featevent-feat">Featured event</div>';
+      const feattext = document.createElement("div");
+      feattext.className = "featevent-feat";
+      feattext.innerHTML = "Featured event";
+      eventElement.appendChild(feattext); //this is vertical title
+      //add feat image
+      const featimage = document.createElement("div");
+      featimage.className = "featevent-image";
+      featimage.innerHTML = `<img
+            src=${this.image}
+            alt=${this.name}
+          />`;
+      eventElement.appendChild(featimage);
+      //add a div to wrap featname and featregister
+      const wrap = document.createElement("div");
+
+      wrap.className = "featevent-info";
+      //add feat name
+      const featname = document.createElement("div");
+      featname.className = "featevent-info__name";
+      featname.innerHTML = `<h3>${this.name}</h3>`;
+      const featdesctiption = document.createElement("div");
+      featdesctiption.className = "featevent-info__name--description";
+      featdesctiption.innerHTML = `<p>${this.description}</p>`;
+      featname.appendChild(featdesctiption);
+      wrap.appendChild(featname);
+      //registre here
+      const registerElement = document.createElement("div");
+      registerElement.className = "featevent-info__register";
+      const btnElement = document.createElement("button");
+      btnElement.className = "featevent-info__register--btn";
+      btnElement.innerHTML = "Register";
+      registerElement.appendChild(btnElement);
+      wrap.appendChild(registerElement);
+      eventElement.appendChild(wrap);
+
+      // eventElement.appendChild(registerElement);
     } else {
       eventElement.className = "event";
-      eventElement.innerHTML = `<div class="event-name">
-            <h3>ICE RINK BY CHARLES BRIDGE</h3>
-          </div>`;
+      const nameElement = document.createElement("div"); //create a new div.event-name
+      nameElement.className = "event-name";
+      nameElement.innerHTML = `<h3>${this.name}</h3>`;
+      eventElement.appendChild(nameElement);
+      const btnElement = document.createElement("button");
+      btnElement.innerHTML = `More`;
+      eventElement.appendChild(btnElement);
     }
     this.element = eventElement;
     return eventElement;
